ce7436e1d39731d69aac693a0fce0ec17829da0a
shreyadhingra12 <shreyadhingra31@gmail.com>
2025-11-17 13:23:07 +0530

    Added- buttons for random colors

---
+++ first/src/components/random_color/index.jsx
@@ -0,0 +1,9 @@
+export default function RandomColor() {
+  return (
+    <div className="container">
+      <button>create HEX color</button>
+      <button>create RGB color</button>
+      <button>Generate Random color</button>
+    </div>
+  );
+}
